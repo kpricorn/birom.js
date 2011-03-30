61288962eef0420a2de011acf81f82ce9fa27000
sebastian de castelberg <github.com@kpricorn.org>
2011-03-30 18:51:12 +0200

    moved node code to server.js and depend fully on npm

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require.paths.unshift(__dirname + "/vendor");
-
 process.addListener('uncaughtException', function (err, stack) {
   console.log('------------------------');
   console.log('Exception: ' + err);
@@ -7,8 +5,52 @@ process.addListener('uncaughtException', function (err, stack) {
   console.log('------------------------');
 });
 
-var Birom = require('./lib/birom');
+var http = require('http'),
+    sys  = require('sys'),
+    static = require('node-static'),
+    faye = require('faye'),
+    url = require('url'),
+    express = require('express'),
+    connect = require('connect');
 
-new Birom({
-  port: 8000
+var app = express.createServer();
+
+
+  //self.bayeux = self.createBayeuxServer();
+
+app.configure( function(){
+
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+
+    //Cookies are not really needed... but may be in the future?
+    app.use(express.cookieParser());
+    app.use(
+        express.session({
+            key: "birom-cookie",
+            secret: "aoeuaoeuaoeu",
+            cookie: { path: '/', httpOnly: true, maxAge: 14400000 }
+        })
+    );      
 });
+
+app.get('/config.json', function(req, res) {
+  res.contentType('application/x-javascript');
+  res.send({port: 8080});
+});
+
+//self.bayeux.attach(app);
+app.listen(8000);
+sys.log('Server started on PORT ' + 8000);
+
+//Birom.prototype.createBayeuxServer = function() {
+//  var self = this;
+//  
+//  var bayeux = new faye.NodeAdapter({
+//    mount: '/faye',
+//    timeout: 45
+//  });
+//  
+//  return bayeux;
+//};
+
